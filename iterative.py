0875db22aa8d7d104df0e38fa9a913feb669e9be
mojtabavi <mojtabavi99@yahoo.com>
2020-05-08 22:47:58 +0430

    Create first method

---
+++ iterative.py
@@ -0,0 +1,27 @@
+#lets do it
+
+import math
+
+def iterative_binarysearch(L,U,arr,x):
+
+    arr.sort()
+
+    
+
+    while(L <= U):
+
+        mid = int((L+U)/2)
+        if(x == arr[mid]):
+            return True
+        if(x > arr[mid]):
+            L = mid + 1
+        else:
+            U = mid -1
+        
+    return False
+        
+
+
+
+    
+
